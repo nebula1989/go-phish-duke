a73994743b2026536ee21a88ed25b412ee8e8b07
nebula919 <benwalterscoding@gmail.com>
2019-09-13 19:27:06 +0000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   resources :phishes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'phishes/new', to: 'phishes#new', as: :phish_form
+  get 'phishes/new', to: 'phishes#new', as: :new_phish
 end
