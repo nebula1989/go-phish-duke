99040b050ff4855d827c6a4d45a7caa474007fb0
nebula919 <benwalterscoding@gmail.com>
2019-09-13 19:24:09 +0000

    made home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
   resources :phishes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'phishes/new', to: 'phishes#new', as: :phish_form
 end
